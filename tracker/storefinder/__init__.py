d87e7d296df5973311075e4293092cd387fca569
alxweimann <alex.weimann@vodafone.de>
2026-07-02 09:35:06 +0200

    Update __init__.py

--- tracker/storefinder/__init__.py
+++ tracker/storefinder/__init__.py
@@ -1,1 +1,9 @@
+"""Storefinder-Infrastruktur für deutschlandweite Filialabfragen."""
 
+from .base import StoreFinderResult
+from .cache import StoreCache
+
+__all__ = [
+    "StoreFinderResult",
+    "StoreCache",
+]
