341c50cb7b1bc86efdb2600c674de074c7ae6295
alxweimann <alex.weimann@vodafone.de>
2026-07-02 09:37:51 +0200

    Delete tracker/tracker/storefinder directory

--- tracker/tracker/storefinder/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Storefinder-Infrastruktur für deutschlandweite Filialabfragen."""
-
-from .base import StoreFinderResult
-from .cache import StoreCache
-
-__all__ = [
-    "StoreFinderResult",
-    "StoreCache",
-]
